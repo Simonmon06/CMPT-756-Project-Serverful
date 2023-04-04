cae2568b1ae810378fa150f390a1d3b43480b22e
Zeye Gu <zeyegu@gmail.com>
2023-04-03 19:01:34 -0700

    implement PostSearch & fix cant fetch post when creating first post due to latency

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,20 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import CommentCreate from "./CommentCreate";
 import CommentList from "./CommentList";
 import PostCreate from "./PostCreate";
+import PostSearch from "./PostSearch";
 const PostList = () => {
-  const [posts, setPosts] = useState({});
+  const [posts, setPosts] = useState([]);
 
-  const fetchPosts = async () => {
-    const res = await axios.get("http://posts.com/posts");
-    console.log('The all query data we have is', res.data)
-    setPosts(res.data);
-  };
+  const fetchPosts = useCallback(async () => {
+    try {
+      const res = await axios.get("http://posts.com/posts");
+      if (res.data.length === 0) {
+        // Retry fetching posts after 500ms if no posts are found
+        setTimeout(fetchPosts, 500);
+      } else {
+        console.log("The all query data we have is", res.data);
+        setPosts(res.data);
+      }
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  }, []);
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [fetchPosts]);
+
+
+
 
   const renderedPosts = Object.values(posts).map((post) => {
     console.log('post.comments, ', post.comments)
@@ -23,7 +36,7 @@ const PostList = () => {
       <div
         className="card"
         style={{ width: "30%", marginBottom: "20px" }}
-        key={post.id}
+        key={post.postId}
       >
         <div className="card-body">
           <h3>{post.title}</h3>
@@ -40,9 +53,14 @@ const PostList = () => {
       <PostCreate onPostCreate={fetchPosts} />
       <hr />
       <h2>All Posts</h2>
-      <div className="d-flex flex-row flex-wrap justify-content-between">
-      {renderedPosts}
-      </div>
+      {posts.length ===0 ? 
+                        <div>Please Make a Post. Waiting...</div>: 
+                        <div className="d-flex flex-row flex-wrap justify-content-between">
+                          {renderedPosts}
+                        </div>
+                        }
+      <h2>Search a Post</h2>
+      <PostSearch/>
     </div>
     
      
